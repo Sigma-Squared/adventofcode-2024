71884d9408e7c28e14eb7611b85c66f2673e0789
Chamu Rajasekera <chamu.rajasekera@gmail.com>
2024-12-05 03:58:34 -0800

    Update day3-p2.js

--- day3-p2.js
+++ day3-p2.js
@@ -1,6 +1,6 @@
 document.body.innerText.matchAll(/(?<op>mul)\((?<x>\d+),(?<y>\d+)\)|(?<op>do)\(\)|(?<op>don't)\(\)/g)
-  .reduce((o, {groups: g}) => ({
-    "mul": () => ({...o, t: o.t+o.do*g.x*g.y}), 
-    "do": () => ({...o, do: true}), 
-    "don't": () => ({...o, do: false})
-  }[g.op]()), {do: true, t: 0}).t
+  .reduce((state, {groups: g}) => ({
+    "mul": (s) => ({...s, t: s.t+s.do*g.x*g.y}), 
+    "do": (s) => ({...s, do: true}), 
+    "don't": (s) => ({...s, do: false})
+  }[g.op](state)), {do: true, t: 0}).t
