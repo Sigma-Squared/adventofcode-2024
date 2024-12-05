52f59d143a073d0afd060042bc3b55952bc469d1
Chamu Rajasekera <chamu.rajasekera@gmail.com>
2024-12-05 02:34:53 -0800

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,9 @@
 Array.prototype.count = function() {
   return this.reduce((acc, n) => (acc[n]=(acc[n] ?? 0) + 1, acc), {});
-}
+};
+Array.prototype.sum = function () {
+  return this.reduce((x, y) => x + y, 0);
+};
+Array.prototype.transpose = function() {
+  return this.reduce((acc, row) => row.map((n, i) => (acc[i] ?? []).concat(n)), []);
+};
